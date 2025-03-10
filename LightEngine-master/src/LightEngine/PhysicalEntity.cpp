83d5161a0fc8d554e5bc535a35706a8c1ba88e79
MaxenceNk <mnkenyi@gaming.tech>
2025-03-10 15:26:13 +0100

    IsColliding

--- LightEngine-master/src/LightEngine/PhysicalEntity.cpp
+++ LightEngine-master/src/LightEngine/PhysicalEntity.cpp
@@ -14,3 +14,16 @@ void PhysicalEntity::Jump()
 	mGravitySpeed -= 100;
 }
 
+bool PhysicalEntity::IsColliding(const AABBCollider& c1, const AABBCollider& c2)
+{
+	if (c1.xMax < c2.xMin || c1.xMin > c2.xMax)
+	{
+		return false;
+	}
+	if (c1.yMax < c2.yMin || c1.yMin > c2.yMax)
+	{
+		return false;
+	}
+	return true;
+}
+
