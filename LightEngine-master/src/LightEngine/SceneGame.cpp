ab250a8a5215819e7f29a396d9e69b8e041fa6b5
RMwinst0n <maroux@gaming.tech>
2025-03-11 11:46:04 +0100

    gvt

--- LightEngine-master/src/LightEngine/SceneGame.cpp
+++ LightEngine-master/src/LightEngine/SceneGame.cpp
@@ -1,14 +1,19 @@
 #include "SceneGame.h"
 #include <iostream>
 #include "Player.h"
-
 #include "Debug.h"
+#include <SFML/Graphics.hpp>
 
 void SceneGame::OnInitialize()
 {
-	pPlayer = CreateEntity<Player>(20, sf::Color::Red);
-	pPlayer->SetPosition(640, 600);
-	pPlayer->SetRigidBody(true);
+    pPlayer = CreateEntity<Player>(20, sf::Color::Red);
+    pPlayer->SetPosition(640, 600);
+    pPlayer->SetRigidBody(true);
+
+    sf::RectangleShape Rect(sf::Vector2f(200.f, 50.f)); 
+    Rect.setFillColor(sf::Color::Blue);
+    Rect.setPosition(300.f, 300.f);
+    
 }
 
 void SceneGame::OnEvent(const sf::Event& event)
@@ -18,6 +23,7 @@ void SceneGame::OnEvent(const sf::Event& event)
 
 void SceneGame::OnUpdate()
 {
-	std::string text = "Life : " + std::to_string(pPlayer->GetLife());
-	Debug::DrawText(10, 10, text, sf::Color::White);
+    std::string text = "Life : " + std::to_string(pPlayer->GetLife());
+    Debug::DrawText(10, 10, text, sf::Color::White);
+
 }
