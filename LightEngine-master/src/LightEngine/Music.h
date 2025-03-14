73f3de432d35356aeb15095ffe531c444ffab5fb
MaxenceNk <mnkenyi@gaming.tech>
2025-03-14 16:13:35 +0100

    sound et music fixed

--- LightEngine-master/src/LightEngine/Music.h
+++ LightEngine-master/src/LightEngine/Music.h
@@ -1,12 +1,12 @@
-//#pragma once
-//#include <SFML/Audio.hpp>
-//
-//class Music
-//{
-//	sf::Music mMusic;
-//
-//public:
-//	void Load(const char* path);
-//	void Play();
-//};
-//
+#pragma once
+#include <SFML/Audio.hpp>
+
+class Music
+{
+	sf::Music mMusic;
+
+public:
+	void Load(const char* path);
+	void Play();
+};
+
