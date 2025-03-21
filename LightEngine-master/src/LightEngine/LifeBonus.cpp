121acb114e30805846e7fb2cef97e6612da8ce9e
Alyssa <amathieu@gaming.tech>
2025-03-21 07:51:05 +0100

    Fixed Sprites

--- LightEngine-master/src/LightEngine/LifeBonus.cpp
+++ LightEngine-master/src/LightEngine/LifeBonus.cpp
@@ -6,7 +6,7 @@ void LifeBonus::OnInitialize()
 {
 	std::map <std::string, sf::Texture>& m = Texture::GetInstance()->textObject;
 	mShape->setTexture(&m["item"], true);
-	mShape->setTextureRect(sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(9, 18)));
+	mShape->setTextureRect(sf::IntRect(sf::Vector2i(9, 0), sf::Vector2i(9, 18)));
 }
 
 void LifeBonus::isCollid(Player* pPlayer)
